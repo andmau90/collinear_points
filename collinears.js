10edc852808c81af59581686371dca2165403a47
Andrea Mauri <mauri.a90@gmail.com>
2019-01-18 17:30:46 +0100

    added getCollinearsSegmensts function

--- collinears.js
+++ collinears.js
@@ -1,42 +1,89 @@
 var exports = (module.exports = {});
 
-var points = [];
+let points = [];
 
-exports.clearSpace = function() {
-  points = [];
+exports.clearSpace = () => {
+    points = [];
 };
 
-exports.getSpace = function() {
-  return points;
+exports.getSpace = () => points;
+
+randomPoint = () => {
+    const a = Math.random() * 2 * Math.PI;
+    const r = 100 * Math.sqrt(Math.random());
+    const x = r * Math.cos(a);
+    const y = r * Math.sin(a);
+    return { x: x.toFixed(0), y: y.toFixed(0) };
 };
 
-exports.addToSpace = function(point) {
-  var message = "";
-  if (point.x && point.y) {
-    message = "added point to space";
-  } else {
-    message = "error on point's coordinates";
-  }
-  return { x: point.x, y: point.y, message: message };
-};
-
-exports.getLines = function(n) {
-  var collinears = {};
-  var pointsCopy = points.slice(0);
-  for (var i = 0; i < pointsCopy.length - 1; i++) {
-    var pointA = pointsCopy[i];
-    for (var j = i + 1; j < pointsCopy.length; j++) {
-      var pointB = pointsCopy[j];
-      var slope = (pointB.y - pointA.y) / (pointB.x - pointA.x);
-      if (!collinears[slope]) {
-        collinears[slope] = [];
-      }
-      collinears[slope].push(pointA, pointB);
+exports.generatePoints = number => {
+    for (var i = 0; i < number; i++) {
+        points.push(randomPoint());
     }
-  }
-  //TODO: algorithm to retrieve collinear lines
-  // > 3 punti
-  //sort
-  //res.json(points);
-  return [];
+    return points;
+};
+
+isNumber = number => typeof number === "number";
+
+exports.addToSpace = ({ x, y }) => {
+    let message = "";
+    if (isNumber(x) && isNumber(y)) {
+        points.push({ x, y });
+        message = "added point to space";
+    } else {
+        message = "error on point's coordinates";
+    }
+    return { x, y, message: message };
+};
+
+exports.addsToSpace = points => {
+    const messages = [];
+    if (points) {
+        for (let i = 0; i < points.length; i++) {
+            messages.push(exports.addToSpace(points[i]));
+        }
+    } else {
+        messages.push("No items");
+    }
+    return messages;
+};
+
+slope = ({ x: x1, y: y1 }, { x: x2, y: y2 }) => {
+    if (x2 != x1) {
+        return y2 - y1 / x2 - x1;
+    }
+    return "oo";
+};
+
+/**
+ * return all available segments with slope
+ */
+getCollinearsSegments = minLength => {
+    const lines = [];
+    for (let i = 0; i < points.length - 1; i++) {
+        let pointA = points[i];
+        for (let j = i + 1; j < points.length; j++) {
+            let pointB = points[j];
+            let line = [pointA, pointB];
+            for (let x = j + 1; x < points.length; x++) {
+                let pointC = points[x];
+                if (
+                    slope(pointA, pointB) === slope(pointB, pointC) &&
+                    slope(pointB, pointC) === slope(pointC, pointA)
+                ) {
+                    line.push(pointC);
+                }
+            }
+            if (line.length >= minLength) {
+                lines.push(line);
+            }
+        }
+    }
+    return lines;
+};
+
+exports.getLines = n => {
+    const lines = getCollinearsSegments(n);
+    lines.sort((a, b) => b.length - a.length);
+    return lines;
 };
--- server.js
+++ server.js
@@ -5,34 +5,42 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
 app.post("/point", function(req, res) {
-  res.json(collinears.addToSpace(req.body));
+    res.json(collinears.addToSpace(req.body));
+});
+
+app.post("/points", function(req, res) {
+    res.json(collinears.addsToSpace(req.body));
 });
 
 app.get("/space", function(req, res) {
-  res.json(collinears.getSpace());
+    res.json(collinears.getSpace());
+});
+
+app.use("/randomize/:number", function(req, res) {
+    res.json(collinears.generatePoints(req.params.number));
 });
 
 app.use("/lines/:number", function(req, res) {
-  res.json(collinears.getLines(req.params.number));
+    res.json(collinears.getLines(req.params.number));
 });
 
 app.delete("/space", function(req, res) {
-  collinears.clearSpace();
-  res.json(collinears.getSpace());
+    collinears.clearSpace();
+    res.json(collinears.getSpace());
 });
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 var server = app.listen(port, hostname, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log("App listening at http://%s:%s", host, port);
+    console.log("App listening at http://%s:%s", host, port);
 });
